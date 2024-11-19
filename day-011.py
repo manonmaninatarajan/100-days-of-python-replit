11a6251be4c5e5a9091159dfad5e7b1e616a5832
Manonmani Natarajan <75035658+manonmaninatarajan@users.noreply.github.com>
2024-11-19 16:23:09 +0200

    Update day-011.py

--- day-011.py
+++ day-011.py
@@ -1,1 +1,7 @@
-print("there are" ,1*(365*24*60*60), "seconds in a year")
+year=int(input("How many days in the year"))
+if year == 365:
+  print("there are" ,1*(year*24*60*60), "seconds in a year")
+elif year ==366:
+  print("there are" ,1*(year*24*60*60), "seconds in a year")
+else:
+  print("please enter a valid days in a year")
