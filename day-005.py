6c7563eebf5a15168435b211949c59d8b074549f
Manonmani Natarajan <75035658+manonmaninatarajan@users.noreply.github.com>
2024-11-19 11:00:26 +0200

    Create day-005.py

---
+++ day-005.py
@@ -0,0 +1,19 @@
+print("Marvel Movie Character Creator")
+print("----")
+hanging=input("""Do you like 'hanging around'?""")
+if hanging == "yes":
+  print("You are Spider-man")
+else:
+  print("Then you're not Spider-man")
+
+voice=input("Do you have a 'gravelly' voice?")
+if voice == "yes":
+  print("You are Korg")
+else:
+  print("Aww, then you're not Korg")
+
+marvel=input("Do you often feel 'Marvelous'?")
+if marvel == "yes":
+  print("Aha! You're Captain Marvel! Hi!")
+else:
+  print("You are not Captain Marvel")
